ebe44695b25d9fd08453cebcdc9cc147385c76b9
BrianED <brian_dooley9@yahoo.ie>
2017-01-24 22:39:39 +0000

    Add files via upload

---
+++ mysite/index.php
@@ -0,0 +1,64 @@
+<!DOCTYPE>
+<html>
+	<head>
+		<title>My shop</title>
+		<link rel="stylesheet" type="text/css" href="styles/style.css" media="all"/>
+	</head>
+	
+<body>
+
+
+	<!--Main Container starts here-->
+	<div class="main_wrapper">
+		
+		<!--Header starts here-->
+		<div class="header_wrapper">
+		
+			<img id="img-1" src="images/logo2.gif" />
+			<img id="img-2" src="images/banner2.gif" />			
+			
+		</div>
+		<!--Header ends here-->
+		
+		<!--Navigation bar starts here-->
+		<div class="menubar">
+			
+			
+			<ul id="menu">
+			
+				<li><a href="#">Home</a></li>
+				<li><a href="#">All Products</a></li>
+				<li><a href="#">My Account</a></li>
+				<li><a href="#">Sign up</a></li>
+				<li><a href="#">Shopping cart</a></li>
+				<li><a href="#">Contact us</a></li>
+			
+			</ul>
+		 </div>
+		<!--Main Container ends here-->
+		
+		<!--Content wrapper starts here-->
+		<div class="content_wrapper">
+		
+			<div id="sidebar">This is sidebar</div>
+		
+			<div id="content_area">This is content area</div>
+		</div>
+		<!--Content wrapper ends here-->
+		
+		<!--Footer starts here-->
+		<div id="footer">
+		This is thefooterd
+		</div>
+		<!--Footer ends here-->
+		
+		
+	</div>
+	<!--Main Container ends here-->
+
+
+
+
+
+</body>
+</html>
